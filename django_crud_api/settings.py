ee18d57d86536a8edd39e52039c9f321fb8b7652
Fabián Poblete <fabian.poblete@cftsa.cl>
2025-02-08 13:43:59 -0300

    vercel configure host

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -128,8 +128,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# cors authorization
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # For local development
+    "https://react-django-kohl.vercel.app",  # Your Vercel domain (without paths)
+]
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
